cec3239f4057b4924ddb7f6b59ab292586a26439
alexkrv <aleksandr.krv@yandex.ru>
2018-10-24 22:56:23 +0300

    Clearing interval for dirty countdown timer was added

--- routes/router.js
+++ routes/router.js
@@ -124,5 +124,22 @@ router.post('/age/set/', function (req, res, next) {
     }
 });
 
+router.get('/user', function (req, res, next) {
+    User.findById(req.session.userId)
+        .exec(function (error, user) {
+            if (error) {
+                return next(error);
+            } else {
+                if (user === null) {
+                    var err = new Error('Not authorized! Go back!');
+                    err.status = 400;
+                    return next(err);
+                } else {
+                    return res.send( user )
+                }
+            }
+        });
+});
+
 
 module.exports = router;
